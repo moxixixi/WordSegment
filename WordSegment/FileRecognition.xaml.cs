5025786f1462128532d8f3ec15a91d0df59d6903
moxixixi <lapzip@sina.com>
2018-03-20 00:17:30 +0800

    [ADD]增加分词功能

--- WordSegment/FileRecognition.xaml.cs
+++ WordSegment/FileRecognition.xaml.cs
@@ -20,9 +20,48 @@ namespace WordSegment
     /// </summary>
     public partial class FileRecognition : Page
     {
+        /// <summary>
+        /// page初始化
+        /// </summary>
         public FileRecognition()
         {
             InitializeComponent();
+
+            SetProgressValue(0);
+        }
+
+        /// <summary>
+        /// 添加文件按钮响应
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog ofd = new Microsoft.Win32.OpenFileDialog();
+            ofd.Filter = "文档 | *.doc; *.docx; *.txt; *.pdf";
+            if (ofd.ShowDialog() == true)
+            {
+                txtBox.Text = ofd.FileName;
+            }
+        }
+
+        /// <summary>
+        /// 进度条值设置
+        /// </summary>
+        /// <param name="nValue"></param>
+        private void SetProgressValue(int nValue)
+        {
+            Pgb.Value = nValue;
+        }
+
+        /// <summary>
+        /// 文件识别按钮响应
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnRecognize_Click(object sender, RoutedEventArgs e)
+        {
+            txtBox.Text = Segment.SegWords("");
         }
     }
 }
---
+++ WordSegment/Segment.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace WordSegment
+{
+    class Segment
+    {
+        /// <summary>
+        /// 利用SCWS进行中文分词
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static string SegWords(string str)
+        {
+            System.Text.StringBuilder sb = new System.Text.StringBuilder();
+            try
+            {
+                string s = string.Empty;
+                System.Net.CookieContainer cookieContainer = new System.Net.CookieContainer();
+                // 将提交的字符串数据转换成字节数组           
+                byte[] postData = System.Text.Encoding.ASCII.GetBytes("data=" + System.Web.HttpUtility.UrlEncode(str) + "&respond=json&charset=utf8&ignore=yes&duality=no&traditional=no&multi=0");
+
+                // 设置提交的相关参数
+                System.Net.HttpWebRequest request = System.Net.WebRequest.Create("http://www.ftphp.com/scws/api.php") as System.Net.HttpWebRequest;
+                request.Method = "POST";
+                request.KeepAlive = false;
+                request.ContentType = "application/x-www-form-urlencoded";
+                request.CookieContainer = cookieContainer;
+                request.ContentLength = postData.Length;
+
+                // 提交请求数据
+                System.IO.Stream outputStream = request.GetRequestStream();
+                outputStream.Write(postData, 0, postData.Length);
+                outputStream.Close();
+
+                // 接收返回的页面
+                System.Net.HttpWebResponse response = request.GetResponse() as System.Net.HttpWebResponse;
+                System.IO.Stream responseStream = response.GetResponseStream();
+                System.IO.StreamReader reader = new System.IO.StreamReader(responseStream, System.Text.Encoding.GetEncoding("utf-8"));
+                string val = reader.ReadToEnd();
+
+                Newtonsoft.Json.Linq.JObject results = Newtonsoft.Json.Linq.JObject.Parse(val);
+                foreach (var item in results["words"].Children())
+                {
+                    Newtonsoft.Json.Linq.JObject word = Newtonsoft.Json.Linq.JObject.Parse(item.ToString());
+                    sb.Append(word["word"].ToString() + " ");
+                }
+            }
+            catch
+            {
+            }
+
+            return sb.ToString();
+        }
+    }
+}
